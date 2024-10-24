0f5be7d999fce63cabf0e8f1404dff2926ee27bf
ReBarbaro <francescovito.dipalo@marconibari.edu.it>
2024-10-24 08:00:26 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,27 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    int N,i;
+    float numero,max;
+    cout<<"quante volte devo inserire i numeri?"<<endl;
+    cin>>N;
+    i=1;
+    while(i<=N){
+        cout<<"scrivi il numero"<<endl;
+        cin>>numero;
+        if(i==1){
+            max=numero;
+            }else{
+                if(numero>max){
+                    max=numero;
+                 }
+                  }
+                  i=i+1;
+                 }
+                 cout<<"il numero massimo é "<<max<<endl;  
+
+
+
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
